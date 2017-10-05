f975cd44d1b87146c858ecba1f1c7dfd5e265643
lnbello10 <ln.bello10@uniandes.edu.co>
2017-10-05 15:28:15 -0500

    cliente

--- Cliente/src/mundo/TCPCliente.java
+++ Cliente/src/mundo/TCPCliente.java
@@ -5,4 +5,25 @@ import java.net.*;
 
 public class TCPCliente {
 
+	public static void main(String argv[]) throws Exception{
+		
+		String frase;
+		String fraseModificada;
+		
+		BufferedReader entradaDesdeUsuario = new BufferedReader(new InputStreamReader(System.in));
+		Socket socketCliente = new Socket("localhost", 9999);
+		
+		DataOutputStream salidaAServidor = new DataOutputStream(socketCliente.getOutputStream());
+		BufferedReader entradaDesdeServidor = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
+		
+		frase=entradaDesdeUsuario.readLine();
+		salidaAServidor.writeBytes(frase+'\n');
+		
+		fraseModificada=entradaDesdeServidor.readLine();
+		System.out.println("del servidor: "+fraseModificada );
+		
+		socketCliente.close();
+		
+	}
+	
 }
