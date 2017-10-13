ce999d04ed146280ec9d6a65e65e5e12bb35be25
lnbello10 <ln.bello10@uniandes.edu.co>
2017-10-12 22:09:56 -0500

    ya crea el archivo

--- ServidorUDP/src/mundo/ServidorUDP.java
+++ ServidorUDP/src/mundo/ServidorUDP.java
@@ -1,11 +1,13 @@
 package mundo;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.util.Date;
-
 import java.sql.Timestamp;
 
 public class ServidorUDP {
@@ -13,43 +15,58 @@ public class ServidorUDP {
 	public static void main(String[] args) throws Exception {
 
 		DatagramSocket socketServidor = new DatagramSocket(9999);
-		
+
 		byte[] recibirDatos = new byte[1024];
 		byte[] enviarDatos = new byte[1024];
+
+		String prueba = "./data/prueba.txt";
+		PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(prueba)),true);
 		
+		
+		//pw.close();
+		
+
 		while(true){
 			
+		
+
 			DatagramPacket recibirPaquete = new DatagramPacket(recibirDatos, recibirDatos.length);
-			
+
 			socketServidor.receive(recibirPaquete);
-			
+
 			String frase = new String(recibirPaquete.getData());
-			
+
 			InetAddress DireccionIP = recibirPaquete.getAddress();
-			
+
 			int puerto = recibirPaquete.getPort();
-			
+
 			String[] fraseN = frase.split(";");
-			
+
 			String fraseNumero = fraseN[0];
-			
+
 			Timestamp marcaTiempoPaquete = Timestamp.valueOf(fraseN[1]);
 			//System.out.println("Marca tiempo paquete: "+marcaTiempoPaquete);
 			long miliSegundos = marcaTiempoPaquete.getTime();
-			
+
 			Timestamp marcaTiempoActual = new Timestamp(System.currentTimeMillis());
 			//System.out.println("Marca tiempo actual: "+marcaTiempoActual);
 			long mseg = marcaTiempoActual.getTime();
-			
+
 			String paraEnviar = fraseNumero+" : "+ (mseg - miliSegundos) +"ms";
-			
+
 			enviarDatos = paraEnviar.getBytes();
 
-			
+			pw.println(paraEnviar);
+
 			DatagramPacket enviarPaquete = new DatagramPacket(enviarDatos, enviarDatos.length,DireccionIP,puerto);
-			
+
 			socketServidor.send(enviarPaquete);
 		}
+	}
+	
+	
+	public void escribirArchivo(){
+		
 	}
 
 }
